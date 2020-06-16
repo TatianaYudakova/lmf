e045803b96f56308bb18d4984a6e6713c07a8431
Tatiana <yudakovatl@gmail.com>
2020-06-17 00:19:52 +0400

    add build

---
+++ build/precache-manifest.d0a8d426c56d520608b0d6c47e4eb74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dea72ea2beeed79f38a3d859566d32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d0ac6466a6632fe1d0",
+    "url": "/static/css/main.5075c84f.chunk.css"
+  },
+  {
+    "revision": "fa1edb29574ed4f447da",
+    "url": "/static/js/2.f3d1383b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3d1383b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d0ac6466a6632fe1d0",
+    "url": "/static/js/main.9bc8dd93.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
